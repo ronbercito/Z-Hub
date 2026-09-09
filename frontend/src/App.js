3f0b70b5815ce2f222f3c2a0542fa6cbb2e9f1a9
ronbercito <rj.ston1985@gmail.com>
2026-09-09 07:49:54 -0500

    fix(theme): cargar estilos de submenús claros

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import { Toaster } from "sonner";
 import "./App.css";
 import "./modules/appearance/panel-theme.css";
 import "./modules/appearance/settings-layout.css";
+import "./modules/appearance/sidebar-theme.css";
 import { bootstrapPanelTheme, getToastTheme } from "./modules/appearance/panelThemes";
 
 bootstrapPanelTheme();
